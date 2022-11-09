cd9e5c4e21248fd668cc8d0ac93b11a34e18bead
leerichie <leerichie@wp.pl>
2022-11-09 18:50:52 +0100

    my-banking-1

---
+++ src/test/java/lee/atm/mybankingapp/MyBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lee.atm.mybankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
